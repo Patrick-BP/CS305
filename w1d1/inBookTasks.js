35320a7f68dc00f070e8a792edadcf438b2a89ca
Patrick <papybp@gmail.com>
2022-02-21 19:30:09 -0600

    w1d1

--- w1d1/inBookTasks.js
+++ w1d1/inBookTasks.js
@@ -60,3 +60,4 @@ alert(alert(1) || 2 || alert(3) ) // 1 , 2
 //AND
 alert(1 && null && 2 ); // null
 alert( alert(1) && alert(2) ); // 1 underfined
+alert(null || 2 && 3 || 4); // 3
---
+++ w1d1/savingBalance.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let prompt = require("prompt-sync")();
+let initialAmount = prompt("Enter the initial amount: ");
+let annualInterestRate = prompt(" Enter the annuel interest Rate: ");
+let numberOfYearsCompound = prompt("Enter number of years of compound: ");
+
