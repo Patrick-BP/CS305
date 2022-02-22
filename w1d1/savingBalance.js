df9f29a9f301a5170e2468294b6abdac30b7c66d
Patrick <papybp@gmail.com>
2022-02-21 21:00:32 -0600

    w1d1

--- w1d1/savingBalance.js
+++ w1d1/savingBalance.js
@@ -1,7 +1,23 @@
+/*                        Defining Table 
+
+          Input                  Processing                            Output
+   --------------------------------------------------------------------------------------------
+       prompt user:           |  compute compound intrest monthly      |   monthly compound
+        - initial Amount      |   - multiply initial amount by         |
+        - annual Intrest Rate |    annual intrest Rate power of number |
+        - number of year to   |    of years compound  minus initial    |              
+            compound          |      amount                            | 
+
+*/          
+
+
 "use strict";
 
-let prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();
 let initialAmount = prompt("Enter the initial amount: ");
-let annualInterestRate = prompt(" Enter the annuel interest Rate: ");
+let annualIntrestRate = prompt("Enter the annuel interest Rate: ");
 let numberOfYearsCompound = prompt("Enter number of years of compound: ");
 
+let monthlyCompound = initialAmount * (1 + (annualIntrestRate)**numberOfYearsCompound)- initialAmount;
+
+console.log(monthlyCompound);
